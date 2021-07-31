b277b25cedfd89c98fd5e657936a1b8c59382ee9
COLE <Laura.Cole@nao.org.uk>
2021-07-31 20:29:37 +0100

    Formatted plot

--- olympics/final.R
+++ olympics/final.R
@@ -1,13 +1,15 @@
 library(tidyverse)
 library(janitor)
 library(dslabs)
+library(png)
+library(cowplot)
 # library(gapminder)
 
 data(gapminder)
 
-tuesdata <- tidytuesdayR::tt_load(2021, week = 31)
-olympics_raw <- tuesdata$olympics
-regions<-tuesdata$regions
+# tuesdata <- tidytuesdayR::tt_load(2021, week = 31)
+# olympics_raw <- tuesdata$olympics
+# regions<-tuesdata$regions
 
 regions <- regions %>% 
   clean_names()
@@ -60,43 +62,63 @@ data_for_plot<-left_join(medals,countries_want,by="country")%>%
   mutate(ratio=medal_total/number_of_entries)%>%
   filter(!is.na(gdp_rank))%>%
   mutate(country = fct_reorder(country, gdp_rank,median))%>%
-  mutate(medal = factor(medal, levels = c("Bronze","Silver","Gold")))%>%
-  mutate(label = fct_inorder(glue::glue(
-    "<span style = 'font-size: 11pt; font-family: \"Apple Color Emoji\"'></span>",
-    "{spacer(3)}**{country}**{spacer(3)}",
-    "<span style = 'font-size: 11pt; font-family: \"Apple Color Emoji\"'></span>"
-  )))
+  mutate(medal = factor(medal, levels = c("Bronze","Silver","Gold")))#%>%
+  # mutate(year=as.factor(year))
 
 small<-data_for_plot%>%select(year,country,ratio)%>%distinct()
 
-ggplot()+
+logo <- readPNG("..\\images\\olympics_logo.png",native=TRUE)
+
+plot<-ggplot()+
   geom_bar(data=data_for_plot,aes(x=year,y=number_of_medals,fill=medal),position="stack", stat="identity") +
-  geom_line(data=small,mapping = aes(x = year, y = ratio*300), size = 2)+
+  geom_line(data=small,mapping = aes(x = year, y = ratio*300), size = 0.5)+
   facet_wrap(~country)+
   scale_y_continuous(name = "Number of medals won",
-                     sec.axis = sec_axis(~./300, name = "Ratio of wins to entries")) +
-  # theme(
-  #   axis.title.y = element_text(color = "grey"),
-  #   axis.title.y.right = element_text(color = "blue"))+
+                     sec.axis = sec_axis(~./300, name = "Ratio of medals to number of entries")) +
+  scale_x_continuous(breaks=seq(2000,2016,by=4))+
   scale_fill_manual(values=c("Bronze"="#CD7F32", "Silver"="#C0C0C0", "Gold"="#D4AF37"))+
   theme(
-    plot.caption=element_text(face="bold"),
-    plot.title=element_text(face="bold"),
-    text = element_text(family = font,color = text_colour, size = 14),
+    text = element_text(family = font, color = text_colour, size = 16),
+    plot.title = element_text(family = font, lineheight = 0.9, hjust=0, size = 20),
+    plot.caption=element_text(family = font),
     plot.background = element_rect(fill = "white"),
     panel.background = element_rect(fill = "white", colour = "white"),
     panel.grid = element_blank(),
     legend.background = element_rect(fill = "white", colour = "white"),
-    axis.text.x =  element_text (angle = 45),
+    legend.position = "bottom",
+    axis.text.x =  element_text (angle = 45, hjust=1),
     axis.ticks.x = element_blank(),
-    # axis.text.y= element_blank(),
+    axis.text.y =  element_text (margin = unit(c(5, 5, 5, 5), "mm")),
     axis.ticks.y = element_blank(),
     panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank()#,
-    # legend.position = "none"
+    panel.grid.minor = element_blank()
   )+
-  xlab("Country")+
+  xlab("")+
   ylab("Number of medals")+
-  facet_wrap(~country)
+  facet_wrap(~country)+
+  # inset_element(logo, 0.95, 0.95, 1.0, 1.0)+
+  labs(title = 'Number of medals won at the summer olympic games between 2000 and 2016*',
+       subtitle = "and the ratio of medals won vs the number of events entered",
+       caption="*For the coutries with the top 12 highest GDP in 2008 | Laura Cole | #TidyTuesday")
 
 #this is a test
+
+
+cow_final<-cowplot::ggdraw(plot) +
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)+
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)
+  cowplot::draw_image(logo,x = 0.1, y = 0.1, width = 0.1,hjust = 0, vjust = 1, halign = 1, valign = 1)
+
+
+save_plot("test.png", cow_final, base_height = 7, base_width = 12.5)
+
